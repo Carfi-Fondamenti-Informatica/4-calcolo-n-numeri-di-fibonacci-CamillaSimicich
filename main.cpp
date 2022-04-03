d2624f9f3e2377137de2ab060a5c94fa8356fc0e
CamillaSimicich <100423042+CamillaSimicich@users.noreply.github.com>
2022-04-03 17:52:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+int main (){
+    int n, a=0, b=1, c;
+    cout <<"inserire un numero intero n" << endl;
+    cin >> n;
+    int k=0;
+    if (n>=2){
+        while (k<n){
+            c= a+b;
+            cout<<c<<endl;
+            b=a;
+            a=c;
+            k++;
 
-int main() {
+        }} else {
+        cout <<"errore"<<endl;
+    }
    return 0;
 }
